28ec9bd27dbef7a5419224fe9a8c42730a6b14ce
NataliyaNikulshina <victory515@yandex.ru>
2023-04-26 10:44:38 +0300

    feat: added gh-pages

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { ChangeEvent, FC, FormEvent, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import stringStyle from './string.module.css';
+
 
 export const StringComponent: React.FC = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [loader, setLoader] = useState(false);
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+
   return (
     <SolutionLayout title="Строка">
-     
+     <form className={stringStyle.form} onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          value={inputValue}
+          maxLength={11}
+        />
+        <Button
+          text="Развернуть"
+          type="submit"
+          isLoader={loader}
+        />
+      </form>
     </SolutionLayout>
   );
 };
